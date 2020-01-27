137741c1c9f4d57b0eed6ea01aefd373cf3a8a3b
yamada <z1ametapi@gmail.com>
2020-01-27 16:35:26 +0900

    npmインストールしてSASS環境構築

---
+++ resources/views/Index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+<title>テンプレ</title>
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+{{-- <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script> --}}
+</head>
+<body>
+<p class="hey">やっほー</p>
+{{-- <script type="text/javascript" src="script.js"></script> --}}
+</body>
+</html>
