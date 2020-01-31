f88c8e2b363112d1119f77447eadd57ce09a1fbb
yamada <z1ametapi@gmail.com>
2020-01-31 15:02:44 +0900

    ログインページのデザイン

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,60 +13,61 @@
 </head>
 <body>
 <div id="app">
-@guest
-        <a class="" href="{{ route('login') }}">{{ __('Login') }}</a>
-    @if (Route::has('register'))
-            <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
-    @endif
-
-@endguest
-
-    <main>
-      <div class="">{{ __('Login') }}</div>
-
-      <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-
-        <label for="email" class="">{{ __('E-Mail Address') }}</label>
-
-
-        <input id="email" type="email" class="{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
+  @include('subviews.sub_header2')
+  <div class="contentsBox">
+  <div class="inContentsBox">
+    <h2>QuickQにログイン</h2>
+    <hr>
+    <form method="POST" action="{{ route('login') }}" id="login">
+      @csrf
+      <div class="inputTextBox">
+        <label for="email" class="emailLabel">email:</label>
+        <p class="typeText">
+          <input id="email" class="{{ $errors->has('email') ? ' is-invalid' : '' }}" type="text" name="email" value="{{ old('email') }}" placeholder="quick@example.com" required  autofocus>
+        </p>
         @if ($errors->has('email'))
           <span class="">{{ $errors->first('email') }}</span>
         @endif
+      </div>
 
-      <label for="password" class="">{{ __('Password') }}</label>
+      <div class="inputPasswordBox">
+        <label for="password" class="passwordLabel">pass:</label>
+        <p class="typePassword">
+          <input id="password" class="{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" name="password" placeholder="" required>
+        </p>
+        <p class="password_request"><a class="" href="{{ route('password.request') }}">パスワードをお忘れですか？</a></p>
+        @if ($errors->has('password'))
+        <span class="" role="alert">
+        <strong>{{ $errors->first('password') }}</strong>
+        </span>
+        @endif
+      </div>
 
-
-      <input id="password" type="password" class="{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-      @if ($errors->has('password'))
-      <span class="" role="alert">
-      <strong>{{ $errors->first('password') }}</strong>
-      </span>
-      @endif
-
-
-          <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
+      <div class="submitBox">
+        <p class="inSubmitBox">
+          <input type="submit" name="id" value="ログイン">
+        </p>
+        <div class="inputCheckBox">
           <label class="" for="remember">
-              {{ __('Remember Me') }}
+            <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+              ログインしたままにする
           </label>
+        </div>
+      </div>
 
-      <button type="submit" class="">
-          {{ __('Login') }}
-      </button>
 
-      @if (Route::has('password.request'))
-          <a class="" href="{{ route('password.request') }}">
-              {{ __('Forgot Your Password?') }}
-          </a>
-      @endif
 
-      </form>
-    </main>
+
+
+
+    <div class="signupBox">
+      <p class="inSignupBox"><a href="{{ route('register') }}">新しいQuickQアカウントを作成</a></p>
+    </div>
+
+    </form>
+
+  </div>
+  </div>
 </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
