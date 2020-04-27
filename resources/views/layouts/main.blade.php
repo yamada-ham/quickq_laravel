3268bb9183009b9020c0edaf27c752df081756a4
yamada <z1ametapi@gmail.com>
2020-04-27 20:20:38 +0900

    作成ボタンデザイン修正

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,8 @@
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
 </head>
 <body>
 <div id='app'>
