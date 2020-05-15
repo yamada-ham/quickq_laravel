0f11c4beb6d620090f208bec20fce9bffa4a0652
yamada <z1ametapi@gmail.com>
2020-05-16 08:30:25 +0900

    トップページに会員登録フォーム追加

--- resources/views/pages/Index.blade.php
+++ resources/views/pages/Index.blade.php
@@ -3,11 +3,74 @@
 <div class="topImgBox">
   <div class="inTopImgBox">
     {{-- <img src = "{{asset('images/quickq_title.png')}}"> --}}
-    <div class="catchCopyInTopImgBox">
-      <div class="inCatchCopyInTopImgBox">
-        <h1>お手軽なアンケート収集</h1>
-        <p>QuickQ(クイックキュー)は、<br />すばやく人々の意見を集めるために作られました。<br />誰でも簡単にアンケートを作成し、回答することができます。</p>
+    <div class="contentsInTopImgBox">
+    <div class="inContentsInTopImgBox">
+      <div class="catchCopyInTopImgBox">
+        <div class="inCatchCopyInTopImgBox">
+          <h1>お手軽なアンケート収集</h1>
+          <p>QuickQ(クイックキュー)は、<br />すばやく人々の意見を集めるために作られました。<br />誰でも簡単にアンケートを作成し、回答することができます。</p>
+        </div>
       </div>
+      <div class="signUpBoxInTopImgBox">
+      <div class="inSignUpBoxInTopImgBox">
+        <form method="POST" action="{{ route('register') }}">
+          @csrf
+          <div class="inputTextBox">
+            <label for="name" class="emailLabel">ユーザー名</label>
+            <p class="emailText">
+              <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" placeholder="Q田Q太郎" required autofocus>
+            @if ($errors->has('name'))
+              <div class="errBox"><p>
+              {{ $errors->first('name') }}
+              </p></div>
+            @endif
+          </div>
+
+          <div class="inputTextBox">
+            <label for="email" class="emailLabel">email:</label>
+            <p class="typeText">
+              <input id="email" type="email"  name="email" value="{{ old('email') }}" required>
+            </p>
+            @if ($errors->has('email'))
+              <div class="errBox"><p>
+              {{ $errors->first('email') }}
+              </p></div>
+            @endif
+          </div>
+
+          <div class="inputPasswordBox">
+            <label for="password" class="passwordlLabel">password:</label>
+            <p class="typePassword">
+              <input type="password" name="password" placeholder="•••••••••">
+            </p>
+            @if ($errors->has('password'))
+              <div class="errBox"><p>
+              {{ $errors->first('password') }}
+              </p></div>
+            @endif
+
+          </div>
+
+          <div class="inputPasswordBox">
+            <label for="password-confirm" class="passwordConfirmlLabel">password(確認のためもう一度):</label>
+            <p class="typePassword">
+              <input id="password-confirm" type="password" name="password_confirmation" placeholder="•••••••••" required>
+            </p>
+            @if ($errors->has('password_confirmation'))
+              <div class="errBox"><p>
+              {{ $errors->first('password_confirmation') }}
+              </p></div>
+            @endif
+           </div>
+          <div class="submitBox">
+          <p class="inSubmitBox">
+            <input type="submit" name="id" value="サインアップ">
+          </p>
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>
     </div>
   </div>
 </div>
